4818709fe01c990efc7118f37479875ab57d47ff
askandseekfoundation <info@askandseekfoundation.org>
2026-06-18 22:56:55 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
- process.env.REACT_APP_SUPABASE_URL,
+process.env.REACT_APP_SUPABASE_URL,
 process.env.REACT_APP_SUPABASE_PUBLISHABLE_KEYY
 );
 
