9d860556b1df4ed253ea9d7537724906b6e20583
askandseekfoundation <info@askandseekfoundation.org>
2026-06-19 00:28:34 -0700

    Update index.js
    Add Admin Route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Admin from './Admin';
+
+const isAdmin = window.location.pathname.startsWith('/admin');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </React.StrictMode>
 );
