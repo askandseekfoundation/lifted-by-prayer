2ab813207cf88e860a856246c79f96725c1f27cb
askandseekfoundation <info@askandseekfoundation.org>
2026-06-19 00:46:19 -0700

    Update App.jsx
    Fix env variable names

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_KEY
 );
 
 const CATEGORIES = [
